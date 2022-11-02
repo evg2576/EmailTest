030a7e79a23d1e79c078f86807b61a0d49c8bc94
evg2576 <24evgeniy03@gmail.com>
2022-11-02 21:03:36 +0300

    123

--- EmailTest/Program.cs
+++ EmailTest/Program.cs
@@ -10,55 +10,55 @@ internal class Program
 
     public static void Send()
     {
-        //MailMessage mail = new MailMessage();
-        //SmtpClient SmtpServer = new SmtpClient("smtp.ethereal.email");
-        //mail.From = new MailAddress("24evgeniy00@gmail.com");
-        //mail.To.Add("24evgeniy03@gmail.com");
-        //mail.Subject = "Test Mail - 1";
-        //mail.Body = "mail with attachment";
+        MailMessage mail = new MailMessage();
+        SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+        mail.From = new MailAddress("24evgeniy00@gmail.com");
+        mail.To.Add("24evgeniy03@gmail.com");
+        mail.Subject = "Test Mail - 1";
+        mail.Body = "mail with attachment";
 
-        //Attachment attachment;
-        //attachment = new Attachment("c:/textfile.txt");
-        //mail.Attachments.Add(attachment);
+        Attachment attachment;
+        attachment = new Attachment("c:/textfile.txt");
+        mail.Attachments.Add(attachment);
 
-        //SmtpServer.Port = 587;
-        //SmtpServer.Credentials = new NetworkCredential("24evgeniy00@gmail.com", "22evgeniy22");
-        //SmtpServer.EnableSsl = true;
-        //SmtpServer.UseDefaultCredentials = false;
+        SmtpServer.Port = 587;
+        SmtpServer.Credentials = new NetworkCredential("24evgeniy00@gmail.com", "qvopvrxbosdagrsd");
+        SmtpServer.EnableSsl = true;
+        SmtpServer.UseDefaultCredentials = false;
 
-        //SmtpServer.Send(mail);
+        SmtpServer.Send(mail);
 
 
-        String SendMailFrom = "24evgeniy00@gmail.com";
-        String SendMailTo = "24evgeniy03@gmail.com";
-        String SendMailSubject = "Email Subject";
-        String SendMailBody = "Email Body";
+        //String SendMailFrom = "24evgeniy00@gmail.com";
+        //String SendMailTo = "24evgeniy03@gmail.com";
+        //String SendMailSubject = "Email Subject";
+        //String SendMailBody = "Email Body";
 
-        try
-        {
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587);
-            SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
-            MailMessage email = new MailMessage();
-            // START
-            email.From = new MailAddress(SendMailFrom);
-            email.To.Add(SendMailTo);
-            email.CC.Add(SendMailFrom);
-            email.Subject = SendMailSubject;
-            email.Body = SendMailBody;
-            //END
-            SmtpServer.Timeout = 5000;
-            SmtpServer.EnableSsl = true;
-            SmtpServer.UseDefaultCredentials = false;
-            SmtpServer.Credentials = new NetworkCredential(SendMailFrom, "qvopvrxbosdagrsd");
-            SmtpServer.Send(email);
+        //try
+        //{
+        //    SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com", 587);
+        //    SmtpServer.DeliveryMethod = SmtpDeliveryMethod.Network;
+        //    MailMessage email = new MailMessage();
+        //    // START
+        //    email.From = new MailAddress(SendMailFrom);
+        //    email.To.Add(SendMailTo);
+        //    email.CC.Add(SendMailFrom);
+        //    email.Subject = SendMailSubject;
+        //    email.Body = SendMailBody;
+        //    //END
+        //    SmtpServer.Timeout = 5000;
+        //    SmtpServer.EnableSsl = true;
+        //    SmtpServer.UseDefaultCredentials = false;
+        //    SmtpServer.Credentials = new NetworkCredential(SendMailFrom, "qvopvrxbosdagrsd");
+        //    SmtpServer.Send(email);
 
-            Console.WriteLine("Email Successfully Sent");
-            Console.ReadKey();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.ToString());
-            Console.ReadKey();
-        }
+        //    Console.WriteLine("Email Successfully Sent");
+        //    Console.ReadKey();
+        //}
+        //catch (Exception ex)
+        //{
+        //    Console.WriteLine(ex.ToString());
+        //    Console.ReadKey();
+        //}
     }
 }
